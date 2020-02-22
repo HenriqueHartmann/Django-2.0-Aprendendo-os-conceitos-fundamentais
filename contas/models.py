aaae4234d504ae0284680c3e36b42d89036fe14f
Hartmann <alemaohartmann@gmail.com>
2020-02-22 15:18:59 -0500

    Modelagem e construção dos CRUDs

--- contas/models.py
+++ contas/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nome
+
 
 class Transacao(models.Model):
     data = models.DateTimeField()
